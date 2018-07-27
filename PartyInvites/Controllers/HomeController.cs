aec2d7efc808d60c05a837cbbbe07481cb73a333
Darren Henderson <darrenh@ccsj.edu>
2018-07-27 18:31:34 -0500

    Creating and rendering a view.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using PartyInvites.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello World";
+            return View("MyView");
         }
     }
 }
